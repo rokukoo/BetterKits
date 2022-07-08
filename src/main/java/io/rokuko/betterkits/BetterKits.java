da41a879632207fa9a7986fbdb6b478db91f3376
Rokuko <1873518094@qq.com>
2022-07-08 15:35:35 +0800

    测试代码2。

--- src/main/java/io/rokuko/betterkits/BetterKits.java
+++ src/main/java/io/rokuko/betterkits/BetterKits.java
@@ -76,7 +76,7 @@ public class BetterKits extends Module {
     }
 
     private void giveKit(CommandSender sender, String[] args) {
-
+        nmzxcmzxczkxckzx
     }
 
     private void removeKit(CommandSender sender, String[] args) {
